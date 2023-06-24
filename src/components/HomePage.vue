c2c4bf021ea63a3e105f9e1cccfaef246ee1c3dc
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2023-06-24 12:48:25 +0800

    i18n changes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,14 +6,10 @@ import HomePageEn from './HomePageEn.vue'
 import HomePageJa from './HomePageJa.vue'
 
 const { locale } = useI18n()
-
-function getLocale() {
-	return locale.value
-}
 </script>
 
 <template>
-	<home-page-zh-cn v-if="getLocale() === 'zh-CN'" />
-	<home-page-en v-if="getLocale() === 'en'" />
-	<home-page-ja v-if="getLocale() === 'ja'" />
+	<home-page-zh-cn v-if="locale === 'zh-CN'" />
+	<home-page-en v-if="locale === 'en'" />
+	<home-page-ja v-if="locale === 'ja'" />
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,11 @@ const route = useRoute()
 const { t, locale } = useI18n()
 
 function currentPageIs(path: string) {
-  return route.name === path
+  return route.path === path
+}
+
+function ariaCurrentPageIs(path: string) {
+  return currentPageIs(path) ? 'page' : undefined
 }
 
 function changeLocale(newLocale: string) {
@@ -22,7 +26,7 @@ export enum NavBarItem {
 
 <template>
   <nav class="navbar sticky-top navbar-expand-lg bg-light mb-2">
-    <div class="container">
+    <div class="container" :lang="locale">
       <router-link class="navbar-brand" to="/">BD4VQK</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,13 +37,13 @@ export enum NavBarItem {
         <ul class="navbar-nav">
           <li class="nav-item">
             <router-link class="nav-link" :class="{ active: currentPageIs('/home') }"
-              :aria-current="currentPageIs('/home') ? 'page' : undefined" to="/home">{{
+              :aria-current="ariaCurrentPageIs('/home')" to="/home">{{
                 t('navbar.home')
               }}</router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link" :class="{ active: currentPageIs('/appendix') }"
-              :aria-current="currentPageIs('/appendix') ? 'page' : undefined" to="/appendix">{{
+              :aria-current="ariaCurrentPageIs('/appendix')" to="/appendix">{{
                 t('navbar.appendix')
               }}</router-link>
           </li>
@@ -49,12 +53,13 @@ export enum NavBarItem {
       <div class="d-flex">
         <ul class="navbar-nav">
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{
-              t('navbar.language') }}</a>
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false"><span lang="zh-CN">语言</span>/<span lang="en">Language</span>/<span
+                lang="ja">言語</span></a>
             <div class="dropdown-menu dropdown-menu-end">
-              <button class="btn btn-link dropdown-item" @click="changeLocale('zh-CN')">简体中文</button>
-              <button class="btn btn-link dropdown-item" @click="changeLocale('en')">English</button>
-              <button class="btn btn-link dropdown-item" @click="changeLocale('ja')">日本語</button>
+              <button class="btn btn-link dropdown-item" @click="changeLocale('zh-CN')" lang="zh-CN">简体中文</button>
+              <button class="btn btn-link dropdown-item" @click="changeLocale('en')" lang="en">English</button>
+              <button class="btn btn-link dropdown-item" @click="changeLocale('ja')" lang="ja">日本語</button>
             </div>
           </li>
         </ul>
